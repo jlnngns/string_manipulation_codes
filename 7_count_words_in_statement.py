9c0f10691edf56281f2b35e5cd2a1aaf5f4aea1a
Julianna Gines <jmgines.12@gmail.com>
2025-03-20 14:33:07 +0800

    Create 7_count_words_in_statement.py

---
+++ 7_count_words_in_statement.py
@@ -0,0 +1,8 @@
+# Ask the user to input a complete statement
+statement = input("Input a complete statement: ")
+
+# Count the number of words
+word_count = len(statement.split())
+
+# Print the result
+print("Output:", word_count)
