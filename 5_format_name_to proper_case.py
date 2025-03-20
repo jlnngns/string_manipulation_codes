16ab5da5e239a380881063708679416503ac6283
Julianna Gines <jmgines.12@gmail.com>
2025-03-20 14:21:34 +0800

    Update and rename format_name_to proper_case.py to 5_format_name_to proper_case.py

---
+++ 5_format_name_to proper_case.py
@@ -0,0 +1,10 @@
+import string
+
+# Ask the user to input their full name
+full_name = input("Input your full name: ")
+
+# Convert to proper case
+proper_name = string.capsword(full_name)
+
+# Print the result
+print("Output:", proper_name)
