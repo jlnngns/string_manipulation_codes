3c0b3fd2d4730c5926976050ce6f1cc023c9ceec
Julianna Gines <jmgines.12@gmail.com>
2025-03-20 14:28:07 +0800

    Create 6_reverse_letter_casing.py

---
+++ 6_reverse_letter_casing.py
@@ -0,0 +1,8 @@
+# Ask the user to input their full name
+full_name = input("Input your full name: ")
+
+# Reverse the casing of each character
+reversed_casing = full_name.swapcase()
+
+# Print the result
+print("Output:", reversed_casing)
